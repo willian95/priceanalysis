8730a4e09cd00c0087eeef9c021094e78bf6926d
patriciax <yively.pa@gmail.com>
2020-06-05 14:20:45 -0400

    admin nav styles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}" >
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" >
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}" >
     <title>PriceAnalysis</title>
