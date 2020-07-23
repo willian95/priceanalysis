9b0bef1692e86271fb7a399d4e2f6711ef34c158
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-23 15:07:21 -0400

    cambios

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}" >
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" >
+    <link rel="stylesheet" href="{{ asset('js/alertify/css/alertify.css') }}" >
+    <link rel="stylesheet" href="{{ asset('js/alertify/css/themes/bootstrap.css') }}" >
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}" >
     <title>PriceAnalysis</title>
 </head>
@@ -16,6 +18,7 @@
 </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="{{ asset('/js/alertify/alertify.min.js') }}"></script>
     <script src="{{ asset('/js/app.js') }}"></script>
     @stack('scripts')
 </body>
