0b057861347335f84036f871c2b71213e2e1f69e
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-07-23 15:10:17 -0400

    cambios

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,6 +19,9 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script src="{{ asset('/js/alertify/alertify.min.js') }}"></script>
+    <script>
+        alertify.set('notifier','position', 'top-right');
+    </script>
     <script src="{{ asset('/js/app.js') }}"></script>
     @stack('scripts')
 </body>
