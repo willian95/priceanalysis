775406c9c8bb2a6760db5f66653cf2f62b03a3b7
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-08-18 11:30:09 -0400

    cambios

--- resources/views/user/post/post.blade.php
+++ resources/views/user/post/post.blade.php
@@ -59,8 +59,8 @@
                                     </div>
                                     
                                     <!--<button type="button" @click="checkDescriptionAndTitle()" class="next"  v-if="description == '' || title == ''">Siguiente-1</button>-->
-                                    <button v-if="description != '' && title != ''" type="button" name="next" class="next action-button" value="Next">Siguiente</button>
-                                    <p class="text-right" v-else>Debes agregar un titulo y una descripción para continuar</p>
+                                    <button type="button" name="next" class="next action-button" value="Next" :disabled="description == '' || title == ''">Siguiente</button>
+                                    
                                 </fieldset>
                                 <fieldset>
                                     <div class="card__shadow">
