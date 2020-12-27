f3586716dbc842268fe895b2b6963a702a5a2294
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2020-12-27 11:49:05 -0400

    cambios

--- resources/views/emails/productProposal.blade.php
+++ resources/views/emails/productProposal.blade.php
@@ -1,3 +1,3 @@
 <p>Han solicitado el siguiente producto: {{ $body }}</p>
 
-<a href="{{ url('/admin/product/index')?emailresponse=\Auth::user()->email }}">Crear producto</a>
+<a href="{{ url('/admin/product/index').'?emailresponse='.\Auth::user()->email }}">Crear producto</a>
