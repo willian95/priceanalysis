e4a69dbcd5895e6fd0d8a6531a5f7caa6e5d06be
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-17 15:25:05 -0400

    cambios

--- resources/views/user/post/post.blade.php
+++ resources/views/user/post/post.blade.php
@@ -506,6 +506,15 @@
                         
                     }
 
+                },
+                isNumberDot(evt) {
+                    evt = (evt) ? evt : window.event;
+                    var charCode = (evt.which) ? evt.which : evt.keyCode;
+                    if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+                        evt.preventDefault();;
+                    } else {
+                        return true;
+                    }
                 }
 
             },
