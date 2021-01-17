817b9299f98ed25506a8f745eee1e1f5afe2b505
willian jesus rodriguez ramirez <rodriguezwillian95@gmail.com>
2021-01-17 15:16:35 -0400

    cambios

--- resources/views/user/partials/navbar.blade.php
+++ resources/views/user/partials/navbar.blade.php
@@ -38,7 +38,7 @@
             </li>
             <li class="nav-item">
                 
-                <a class="nav-link btn-general" href="{{ url('/') }}">Login</a>
+                <a class="nav-link btn-general" href="{{ url('/login') }}">Login</a>
             </li>
         @endif
         </ul>
